0d9bac7f3c3c66eee17c891e20313931b4b5faf0
Warek <alexandrudobrojan@gmail.com>
2020-10-05 18:33:10 +0300

    Server side improvements

---
+++ src/ts/app.ts
@@ -0,0 +1,19 @@
+import express = require("express");
+import chalk = require("chalk");
+import fs = require("fs");
+import bodyParser = require("body-parser");
+import cors = require("cors");
+import path = require("path");
+
+const app = express();
+const tasks = JSON.parse( String( fs.readFileSync(path.join(__dirname, "tasks.txt")) ) );
+
+app.use(cors(), express.static(path.resolve(__dirname, "../")));
+
+app.route("/tasks").get((req, res, next) => {
+
+}).post(bodyParser(), (req, res, next) => {
+
+});
+
+app.listen(8000);
