05facd7ff2a697624518b828187c0c39ae5d2adb
Warek <alexandrudobrojan@gmail.com>
2020-10-05 20:49:50 +0300

    Fixes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,7 +6,7 @@ import cors = require("cors");
 import path = require("path");
 
 const app = express();
-const tasks = JSON.parse( String( fs.readFileSync(path.join(__dirname, "tasks.txt")) ) );
+const tasks = JSON.parse( String( fs.readFileSync(path.join(__dirname, "tasks.json")) ) );
 
 app.use(cors(), express.static(path.resolve(__dirname, "../")));
 
