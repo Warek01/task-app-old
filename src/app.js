539d42912022ae8f69d4b452eace245500f9eb6a
Warek <alexandrudobrojan@gmail.com>
2020-10-30 17:03:42 +0200

    History page added

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ const chalk = require("chalk");
 const fs = require("fs");
 const cors = require("cors");
 const path = require("path");
-const app = express();
-let tasks = JSON.parse(fs.readFileSync(path.join(__dirname, "tasks.json")).toString());
+const app = express(), task_path = path.join(__dirname, "tasks.json"), task_history_path = path.join(__dirname, "tasks_history.json");
+/** Tasks database object */
+let tasks = JSON.parse(fs.readFileSync(task_path).toString());
 nodeArgs();
 app.use(cors());
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//       /* / */ ---------------
 app.route("/").get((req, res, next) => {
     try {
-        res.render(path.join(__dirname, "views", "index.ejs"), { tasks: tasks });
+        res.render("index", { tasks: tasks });
     }
     catch (error) {
         serverError(res, error);
@@ -21,11 +24,13 @@ app.route("/").get((req, res, next) => {
 }).post(express.json({ strict: true }), express.urlencoded({ extended: true }), (req, res, next) => {
     try {
         // Get request body (task)
-        let body = req.body;
+        let body = req.body, tasksHistory = JSON.parse(fs.readFileSync(task_history_path, "utf-8"));
         // Push gotten task to local memory tasks array
+        tasksHistory.push(body);
         tasks.push(body);
         // Overwrite static memory file with local memory tasks array
-        fs.writeFileSync(path.join(__dirname, "tasks.json"), JSON.stringify(tasks, null, 2));
+        fs.writeFileSync(task_path, JSON.stringify(tasks, null, 2));
+        fs.writeFile(task_history_path, JSON.stringify(tasksHistory, null, 2), () => { });
     }
     catch (error) {
         serverError(res, error);
@@ -33,12 +38,13 @@ app.route("/").get((req, res, next) => {
     }
     res.end("Success");
 }).delete((req, res, next) => {
+    // Task deleting logic
     try {
         let index = Number(req.query.index);
         // Remove item from array
         tasks.splice(index, 1);
         // Overwrite static memory file with local memory tasks array
-        fs.writeFileSync(path.join(__dirname, "tasks.json"), JSON.stringify(tasks, null, 2));
+        fs.writeFileSync(task_path, JSON.stringify(tasks, null, 2));
     }
     catch (error) {
         serverError(res, error);
@@ -46,7 +52,34 @@ app.route("/").get((req, res, next) => {
     }
     res.sendStatus(200);
 });
+// --------------------------
+//       /* /HISTORY */ ---------------
+app.route("/history").get((req, res, next) => {
+    try {
+        fs.readFile(task_history_path, { encoding: "utf-8" }, (err, data) => {
+            res.render("history", { tasks: JSON.parse(data) });
+            if (err)
+                throw err;
+        });
+    }
+    catch (error) {
+        serverError(res, error);
+    }
+}).delete((req, res, next) => {
+    try {
+        fs.writeFileSync(task_history_path, "[\n\n]");
+    }
+    catch (error) {
+        serverError(res, error);
+        return;
+    }
+    res.sendStatus(200);
+});
+// --------------------------
 app.listen(nodeArgs().port, nodeArgs().port_log);
+app.use(express.static(path.resolve(__dirname, "../")));
+// --------------------------
+/* Functions */
 // Console logger
 function log(req, res, next) {
     console.log(chalk.hex("#74b9ff")("Request got for "), chalk.hex("#A3CB38")(req.url));
@@ -74,16 +107,19 @@ function nodeArgs() {
     // -p [port] or --port [port]
     return {
         port: argv.port || argv.p ? argv.port || argv.p : 8000,
-        /** Logs in console current listened port
-         * @returns Nothing
-         */
         port_log() {
             console.log('"' + path.parse(__filename).base + '"' + " is listening to port " + chalk.hex("#ED4C67")(argv.port || argv.p ? argv.port || argv.p : 8000));
         }
     };
 }
+/**
+ * Logs in console whenever an error on server side
+ * occurs and sends response with status 500
+ * "Internal Server Error"
+ * @param res Response object of current callback
+ * @param error Error object from "try catch" block
+ * */
 function serverError(res, error) {
     console.error(chalk.hex("#e74c3c")("Error: "), chalk.hex("#ff7979")(error));
     res.sendStatus(500);
 }
-app.use(express.static(path.resolve(__dirname, "../")));
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,22 +1,26 @@
 import express = require("express");
 import chalk = require("chalk");
 import fs = require("fs");
-import bodyParser = require("body-parser");
 import cors = require("cors");
 import path = require("path");
 import ejs = require("ejs");
 
-const app = express();
+const app = express(),
+   task_path: string = path.join(__dirname, "tasks.json"),
+   task_history_path: string = path.join(__dirname, "tasks_history.json");
 
-let tasks: any = JSON.parse(fs.readFileSync(path.join(__dirname, "tasks.json")).toString());
+/** Tasks database object */
+let tasks: any = JSON.parse(fs.readFileSync(task_path).toString());
 
 nodeArgs();
 app.use(cors());
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+//       /* / */ ---------------
 app.route("/").get((req, res, next): void => {
    try {
-      res.render(path.join(__dirname, "views", "index.ejs"), {tasks: tasks});
+      res.render("index", {tasks: tasks});
    } catch (error) {
       serverError(res, error);
       return;
@@ -24,11 +28,16 @@ app.route("/").get((req, res, next): void => {
 }).post(express.json({strict: true}), express.urlencoded({extended: true}), (req, res, next): void => {
    try {
       // Get request body (task)
-      let body: {content: string, timestamp: string} = req.body;
+      let body: {content: string, timestamp: string} = req.body,
+         tasksHistory: any = JSON.parse(fs.readFileSync(task_history_path, "utf-8"));
+
       // Push gotten task to local memory tasks array
+      tasksHistory.push(body);
       tasks.push(body);
+
       // Overwrite static memory file with local memory tasks array
-      fs.writeFileSync(path.join(__dirname, "tasks.json"), JSON.stringify(tasks, null, 2));
+      fs.writeFileSync(task_path, JSON.stringify(tasks, null, 2));
+      fs.writeFile(task_history_path, JSON.stringify(tasksHistory, null, 2), () => {});
 
    } catch(error) {
       serverError(res, error);
@@ -36,12 +45,34 @@ app.route("/").get((req, res, next): void => {
    }
    res.end("Success");
 }).delete((req, res, next) => {
+   // Task deleting logic
    try {
       let index: number = Number(req.query.index);
       // Remove item from array
       tasks.splice(index, 1);
       // Overwrite static memory file with local memory tasks array
-      fs.writeFileSync(path.join(__dirname, "tasks.json"), JSON.stringify(tasks, null, 2));
+      fs.writeFileSync(task_path, JSON.stringify(tasks, null, 2));
+   } catch(error) {
+      serverError(res, error);
+      return;
+   }
+   res.sendStatus(200);
+});
+// --------------------------
+
+//       /* /HISTORY */ ---------------
+app.route("/history").get((req, res, next) => {
+   try {
+      fs.readFile(task_history_path, {encoding: "utf-8"}, (err, data) => {
+         res.render("history", {tasks: JSON.parse(data)});
+         if (err) throw err;
+      });
+   } catch(error) {
+      serverError(res, error);
+   }
+}).delete((req, res, next) => {
+   try {
+      fs.writeFileSync(task_history_path, "[\n\n]");
    } catch(error) {
       serverError(res, error);
       return;
@@ -49,7 +80,13 @@ app.route("/").get((req, res, next): void => {
    res.sendStatus(200);
 });
 
+// --------------------------
+
 app.listen(nodeArgs().port, nodeArgs().port_log);
+app.use(express.static(path.resolve(__dirname, "../")));
+
+// --------------------------
+/* Functions */
 
 // Console logger
 function log(req: any, res: any, next: any): void {
@@ -81,18 +118,20 @@ function nodeArgs(): {port: string, port_log(): void} {
    // -p [port] or --port [port]
    return {
       port: argv.port || argv.p? argv.port || argv.p : 8000,
-      /** Logs in console current listened port
-       * @returns Nothing
-       */
       port_log(): void {
          console.log('"' + path.parse(__filename).base + '"' + " is listening to port " + chalk.hex("#ED4C67")(argv.port || argv.p? argv.port || argv.p : 8000));
       }
    };
 }
 
+/** 
+ * Logs in console whenever an error on server side
+ * occurs and sends response with status 500
+ * "Internal Server Error"
+ * @param res Response object of current callback
+ * @param error Error object from "try catch" block
+ * */ 
 function serverError(res: any, error: string): void {
    console.error(chalk.hex("#e74c3c")("Error: "), chalk.hex("#ff7979")(error));
    res.sendStatus(500);
 }
-
-app.use(express.static(path.resolve(__dirname, "../")));
